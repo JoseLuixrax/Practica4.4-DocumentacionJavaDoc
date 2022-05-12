7c125dfb5eb61040bdf91fa5c39a4adfa5e374dc
JoseLuixrax <91657266+JoseLuixrax@users.noreply.github.com>
2022-05-12 09:01:55 +0200

    Delete doc directory

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"gestisimal.classes"},{"l":"gestisimal.exceptions"},{"l":"gestisimal.tests"},{"l":"gestisimal.util"}];updateSearchResults();
